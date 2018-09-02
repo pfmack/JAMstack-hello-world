abc22153c3ded81e15681cae2d9aef8fab23541c
pfmack <mack.precious@gmail.com>
2018-09-02 15:49:29 -0400

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi! I'm Precious.</h1>
+	<p>My goal is to create beautiful, user centered websites and apps. </p>
+		<p>In addition to designing and coding websites, I'm also a librarian that geeks continous learning, Medieval History, WordPress, museums, DIY crafts, and coffee. I seriously love coffee (and code and books).</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
